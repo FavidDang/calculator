ad3b08bbcaf715f2e61d6e424312ca6934f28e6f
dfang <davidfang987@gmail.com>
2024-01-31 19:48:18 -0500

    basic functions

---
+++ script.js
@@ -0,0 +1,9 @@
+function add(n1, n2) { return n1 + n2; }
+function subtract(n1, n2) { return n1 - n2; }
+function multiply(n1, n2) { return n1 * n2; }
+function divide(n1, n2) { return n1 / n2; }
+function operate(n1, n2, op) { return op(n1, n2)}
+
+let n1;
+let n2;
+let op;
